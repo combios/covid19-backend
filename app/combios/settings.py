f3d05455e90c25539202638fc9718b52dde014b9
wjforero <1499698+wjforero@users.noreply.github.com>
2020-03-28 17:51:44 -0500

    Events API

--- app/combios/settings.py
+++ app/combios/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'base',
     'definitions',
+    'events',
 ]
 
 MIDDLEWARE = [
--- app/events/models.py
+++ app/events/models.py
@@ -4,6 +4,9 @@ class QuestionnaireResponse(models.Model):
     questionnaire = models.IntegerField()
     authored = models.DateTimeField()
 
+    def __str__(self):
+        return super().__str__()
+
 class QuestionnaireResponseItem(models.Model):
     response = models.ForeignKey(QuestionnaireResponse, on_delete=models.CASCADE, related_name="answers")
     code = models.SlugField()
