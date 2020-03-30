b78664bd962223593d3073a88f0a717faf87b7e1
Carlos Arbey Jiménez Vidal <arbeyjimenez@MacBook-Pro-de-Carlos-2.local>
2020-03-29 19:57:18 -0500

    Protected apis

--- app/definitions/views.py
+++ app/definitions/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from django_cognito_jwt import JSONWebTokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from .models import *
 from .serializers import *
@@ -7,7 +9,11 @@ from .serializers import *
 class QuestionnaireViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Questionnaire.objects.all()
     serializer_class = QuestionnaireSerializer
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
     
 class QuestionnaireItemViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = QuestionnaireItem.objects.all()
     serializer_class = QuestionnaireItemSerializer
+    authentication_classes = (JSONWebTokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
