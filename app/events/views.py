dd893fc2acef530ff41f159f925e6b006ad35237
wjforero <1499698+wjforero@users.noreply.github.com>
2020-04-07 17:14:53 -0500

    Add create endpoint for questionnaire responses

--- app/events/views.py
+++ app/events/views.py
@@ -1,9 +1,34 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status, mixins
+from rest_framework.response import Response
 
 from .models import *
 from .serializers import *
 
-class QuestionnaireResponseViewSet(viewsets.ReadOnlyModelViewSet):
+class QuestionnaireResponseViewSet(mixins.CreateModelMixin, 
+                   mixins.RetrieveModelMixin, 
+                   #mixins.UpdateModelMixin,
+                   #mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
     queryset = QuestionnaireResponse.objects.all()
     serializer_class = QuestionnaireResponseSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        answers = request.data["answers"]
+        del data["answers"]
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        qr = QuestionnaireResponse.objects.get(pk=serializer.data["id"])
+        for code in answers:
+            a = QuestionnaireResponseItem()
+            a.code = code
+            a.value = answers[code]
+            a.response = qr
+            a.save()
+        headers = self.get_success_headers(serializer.data)
+        serializer.data["answers"] = answers
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
